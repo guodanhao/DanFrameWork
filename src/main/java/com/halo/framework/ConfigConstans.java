1995fda55bd5044528ac24c2a294c148bb72621f
muxi <muxi@2dfire.com>
2016-08-11 19:30:46 +0800

    初始化halo

---
+++ src/main/java/com/halo/framework/ConfigConstans.java
@@ -0,0 +1,10 @@
+package com.halo.framework;
+
+/**
+ * Created by muxi on 2016/8/11.
+ */
+public interface ConfigConstans {
+
+    String CONFIG_FILE = "halo.properties";
+    String JDBC_DRIVER = "halo.framework.jdbc.driver";
+}
