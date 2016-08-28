ffc0552bcebd9a32f304cfe65d395913014547ee
guodanhao <a88494585>
2016-08-28 22:39:03 +0800

    增加ConfigHelper

--- src/main/java/com/halo/framework/ConfigConstans.java
+++ src/main/java/com/halo/framework/ConfigConstans.java
@@ -7,4 +7,11 @@ public interface ConfigConstans {
 
     String CONFIG_FILE = "halo.properties";
     String JDBC_DRIVER = "halo.framework.jdbc.driver";
+    String JDBC_URL = "halo.framework.jdbc.url";
+    String JDBC_NAME = "halo.framework.jdbc.name";
+    String JDBC_PASSWORD = "halo.framework.jdbc.password";
+    String APP_BASE_PACKAGE = "halo.framework.app.base_package";
+    String APP_JSP_PATH = "halo.framework.app.jsp_bath";
+    String APP_ASSET_PATH = "halo.framework.app.assert_path";
+
 }
---
+++ src/main/java/com/halo/framework/ConfigHelper.java
@@ -0,0 +1,35 @@
+package com.halo.framework;
+
+import com.halo.framework.utils.PropsUtils;
+
+import java.util.Properties;
+
+/**
+ * Created by guodanhao on 2016/8/28.
+ */
+public class ConfigHelper {
+
+    private static final Properties CONFIG_PROPS = PropsUtils.loadProps(ConfigConstans.CONFIG_FILE);
+
+    public static String getJdbcUsername(){
+        return PropsUtils.getString(CONFIG_PROPS, ConfigConstans.JDBC_NAME);
+    }
+
+    public static String getJdbcPassword(){
+        return PropsUtils.getString(CONFIG_PROPS, ConfigConstans.JDBC_PASSWORD);
+    }
+
+    public static String getAppBasePackage(){
+        return PropsUtils.getString(CONFIG_PROPS, ConfigConstans.APP_BASE_PACKAGE);
+    }
+
+    public static String getAppAssertPath(){
+        return PropsUtils.getString(CONFIG_PROPS, ConfigConstans.APP_ASSET_PATH);
+    }
+
+    public static String getJspPath(){
+        return PropsUtils.getString(CONFIG_PROPS, ConfigConstans.APP_JSP_PATH);
+    }
+
+
+}
