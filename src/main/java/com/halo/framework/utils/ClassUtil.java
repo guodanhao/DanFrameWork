ef4d2b44852bccf3cc449b84532a55a58b6a3cc7
muxi <muxi@2dfire.com>
2016-09-07 19:44:25 +0800

    增加工具类

--- src/main/java/com/halo/framework/utils/ClassUtil.java
+++ src/main/java/com/halo/framework/utils/ClassUtil.java
@@ -1,9 +1,16 @@
 package com.halo.framework.utils;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by muxi on 2016/9/2.
  */
 public final class ClassUtil {
+
 
     /**
      * 获得当前线程中的类加载器
@@ -23,5 +30,38 @@ public final class ClassUtil {
         return cls;
     }
 
+    public static Class<?> loadClass(String className) {
+        Class<?> cls;
+        try {
+            cls = Class.forName(className);
+        } catch (ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
+        return cls;
+    }
 
+    public static Set<Class<?>> getClassSet(String packageName) {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        try {
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+
+            if (urls.hasMoreElements()) {
+
+                URL url = urls.nextElement();
+
+                //TODO 未完待续
+                if (url != null){
+                    String protocol = url.getProtocol();
+                    if ("file".equals(protocol) ){
+
+                    }else if ("jar".equals(protocol)){
+
+                    }
+                }
+            }
+        } catch (IOException e) {
+
+        }
+        return null;
+    }
 }
