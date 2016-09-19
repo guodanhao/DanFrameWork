e6e7527e3bd3a41ccdd3dd972e31c53e4c91406b
guodanhao <guodh_1@163.com>
2016-09-19 22:51:41 +0800

    修改dispatcherServlet

--- src/main/java/com/halo/framework/DispatcherServlet.java
+++ src/main/java/com/halo/framework/DispatcherServlet.java
@@ -1,11 +1,16 @@
 package com.halo.framework;
 
+import com.halo.framework.helper.ConfigHelper;
 import com.halo.framework.helper.HelperLoader;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by guodanhao on 16-9-19.
@@ -23,13 +28,30 @@ public class DispatcherServlet extends HttpServlet {
 
         HelperLoader.init();
 
+        /**
+         * servlet这边有个回调函数
+         */
         ServletContext servletContext = servletConfig().getServletContext();
 
-        // TODO
+        ServletRegistration jspServlet = servletContext.getServletRegistration("jsp");
 
+        jspServlet.addMapping(ConfigHelper.getJspPath() + "*");
+
+        ServletRegistration defaultServlet = servletContext.getServletRegistration("default");
+
+        defaultServlet.addMapping(ConfigHelper.getAppAssertPath() + "*");
     }
 
     private ServletConfig servletConfig() {
+
         return null;
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String requestMethod = req.getMethod().toLowerCase();
+
     }
 }
